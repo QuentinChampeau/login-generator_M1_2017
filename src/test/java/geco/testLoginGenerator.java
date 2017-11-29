0270a2ac6d44e499f9dbb95d565ba0a33a81f18c
Hearner <quentin.champeau@etud.univ-pau.fr>
2017-11-29 16:59:54 +0100

    test logingenerator

--- src/test/java/geco/testLoginGenerator.java
+++ src/test/java/geco/testLoginGenerator.java
@@ -1,6 +1,34 @@
+package geco;
+import geco.LoginService;
+import geco.LoginGenerator;
+import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class testLoginGenerator {
-    LoginService loginService = new LoginService(new String[] {"JROL", "BPER",
+    private LoginService loginService = new LoginService(new String[] {"JROL", "BPER",
             "CGUR", "JDUP", "JRAL", "JRAL1"});
+
+    @Test
+    public void testGenerateLoginForDurandAndPaul() {
+        LoginGenerator logiGenerator = new LoginGenerator(loginService);
+        String PDUR = logiGenerator.generateLoginForNomAndPrenom("Durand", "Paul");
+        assertThat(PDUR, is("PDUR"));
+    }
+
+    @Test
+    public void testGenerateLoginForRollingAndJean() {
+        LoginGenerator logiGenerator = new LoginGenerator(loginService);
+        String JROL = logiGenerator.generateLoginForNomAndPrenom("Rolling", "Jean");
+        assertThat(JROL, is("JROL"));
+    }
+
+    @Test
+    public void testGenerateLoginForDurand2AndPaul() {
+        LoginGenerator logiGenerator = new LoginGenerator(loginService);
+        String PDUR2 = logiGenerator.generateLoginForNomAndPrenom("Durand", "Paul");
+        assertThat(PDUR2, is("PDUR2"));
+    }
+
 }
